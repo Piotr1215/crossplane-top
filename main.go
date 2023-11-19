de195c5940e4bb958cc198db2b0e1bc92423a0c0
Piotr Zaniewski <piotr@upbound.io>
2023-11-20 00:17:55 +0100

    Get metrics on provider pods

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -20,7 +22,7 @@ func main() {
 	}
 
 	// Create the clientset for Kubernetes
-	_, err = kubernetes.NewForConfig(config)
+	k8sClientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -31,20 +33,37 @@ func main() {
 		panic(err.Error())
 	}
 
-	// Fetch node metrics
-	nodeMetricsList, err := metricsClientset.MetricsV1beta1().NodeMetricses().List(context.TODO(), metav1.ListOptions{})
+	// Fetch all pods in the Crossplane namespace
+	pods, err := k8sClientset.CoreV1().Pods("crossplane-system").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// Print details
-	for _, nodeMetric := range nodeMetricsList.Items {
-		fmt.Printf("Node Name: %s\n", nodeMetric.Name)
-		fmt.Printf("CPU Usage: %s\n", nodeMetric.Usage.Cpu().String())
-		fmt.Printf("Memory Usage: %s\n", nodeMetric.Usage.Memory().String())
+	// Filter pods based on label key prefix
+	for _, pod := range pods.Items {
+		for labelKey := range pod.GetLabels() {
+			if strings.HasPrefix(labelKey, "pkg.crossplane.io/provider") {
+				// Get metrics for this pod
+				podMetrics, err := metricsClientset.MetricsV1beta1().PodMetricses(pod.Namespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+				if err != nil {
+					fmt.Printf("Error getting metrics for pod %s: %v\n", pod.Name, err)
+					continue
+				}
+
+				// Print pod metrics
+				fmt.Printf("Pod Name: %s\n", podMetrics.Name)
+				for _, container := range podMetrics.Containers {
+					fmt.Printf("Container Name: %s\n", container.Name)
+					fmt.Printf("CPU Usage: %dm\n", container.Usage.Cpu().ScaledValue(resource.Milli))
+					fmt.Printf("Memory Usage: %dMi\n", container.Usage.Memory().ScaledValue(resource.Mega))
+				}
+				fmt.Println("-----")
+			}
+		}
 	}
 }
 
+// homeDir returns the home directory for the current user
 func homeDir() string {
 	if h := os.Getenv("HOME"); h != "" {
 		return h
